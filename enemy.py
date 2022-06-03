c1ac8d3ed9c25dd7753b275a023656d7e53853db
fr0stfree <frostfree@inbox.ru>
2022-06-03 11:13:50 +0300

    enemy direction changer added

--- enemy.py
+++ enemy.py
@@ -5,7 +5,7 @@ import settings
 
 
 ENEMY_SIZE = (60, 40)
-
+ENEMY_SPEED = 1
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, position):
@@ -13,3 +13,21 @@ class Enemy(pygame.sprite.Sprite):
         loaded_image = pygame.image.load(os.path.join('graphics', 'enemy_ship.png')).convert_alpha()
         self.image = pygame.transform.scale(loaded_image, ENEMY_SIZE)
         self.rect = self.image.get_rect(center=position)
+        self.speed = ENEMY_SPEED
+
+    def update(self):
+        self.rect.x += self.speed
+
+
+def setup_enemies(group, rows=3, columns=10, x_gap=65, y_gap=50, x_offset=50, y_offset=50):
+    for i, row in enumerate(range(rows)):
+        for j, column in enumerate(range(columns)):
+            position = (j*x_gap+x_offset, i*y_gap+y_offset)
+            enemy_sprite = Enemy(position=position)
+            group.add(enemy_sprite)
+
+def change_enemy_direction(enemies):
+    for enemy in enemies:
+        if enemy.rect.right >= settings.WIDTH or enemy.rect.left <= 0:
+            for enemy in enemies:
+                enemy.speed *= -1
--- main.py
+++ main.py
@@ -3,25 +3,21 @@ import pygame
 
 import settings
 from player import Player
-from enemy import Enemy
+from enemy import setup_enemies, change_enemy_direction
 
 class Game:
     def __init__(self):
         self.player = pygame.sprite.GroupSingle(Player())
 
         self.enemies = pygame.sprite.Group()
-        self.enemies_setup()
-
-    def enemies_setup(self, rows=3, columns=15, x_gap=50, y_gap=50, x_offset=50, y_offset=50):
-        for i, row in enumerate(range(rows)):
-            for j, column in enumerate(range(columns)):
-                position = (j*y_gap+y_offset, i*x_gap+x_offset)
-                enemy_sprite = Enemy(position=position)
-                self.enemies.add(enemy_sprite)
+        setup_enemies(self.enemies)
 
 
     def run(self):
         self.player.update()
+
+        self.enemies.update()
+        change_enemy_direction(self.enemies)
 
         self.player.draw(screen)
         self.player.sprite.missiles.draw(screen)
